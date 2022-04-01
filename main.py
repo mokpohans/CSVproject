cfd1e0a17b091758df14868b1dfbd8a4948a3216
AlexArno <zeunjiz86@gmail.com>
2022-04-01 22:19:30 +0900

    간단한 기능 구현 및 GUI구현중

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
 # CSV파일 선택하기
-
 import os
 import pandas as pd
+import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 
 def file_rode():
-    list_file = []                                          #파일 목록 담을 리스트 생성
-    files = filedialog.askopenfilenames(initialdir="/",title = "파일을 선택 해 주세요",filetypes = (("*.csv","*csv"), ("*.xlsx","*xlsx"), ("*.xls","*xls")))
-    #files 변수에 선택 파일 경로 넣기
+    root.withdraw()                              #파일 목록 담을 리스트 생성
+    file_path = filedialog.askopenfilename(parent=root,
+                                           initialdir="/",
+                                           title='Please select a directory')
+    print(file_path)
+    return file_path
 
-    if files == '':
-        messagebox.showwarning("경고", "파일을 추가 하세요")    #파일 선택 안했을 때 메세지 출력
-    return files
-__tcsv = pd.read_csv(file_rode())
-print(__tcsv)
+
+def creatv_csv():
+    __tcsv = pd.DataFrame(pd.read_csv('C:/Users/zeunj/Downloads/PrefixA1.rawwave.csv'))
+    __tcsv = __tcsv.drop(['Time'], axis=1)
+    _Tcsv = __tcsv.loc[0:199]
+    E = 399
+    F = 200
+    for _ in range(0, 5):
+        a = __tcsv.loc[F:E]
+        _Tcsv = pd.concat([_Tcsv, a.reset_index(drop=True)],axis=1)
+        E = E + 200
+        F = F + 200
+    print(_Tcsv)
+
+root = tk.Tk()
+
+lbl1 = tk.Label(root, text="행",  width=10)
+lbl2 = tk.Label(root, text="열",  width=10)
+lbl1.grid(row=0, column=0)
+lbl2.grid(row=1, column=0)
+txt1 = tk.Entry(root)
+txt2 = tk.Entry(root)
+txt1.grid(row=0, column=1)
+txt2.grid(row=1, column=1)
+btn = tk.Button(root, text="OK", width=15)
+btn.grid(row=2, column=1)
+
+root.mainloop()

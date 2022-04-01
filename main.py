d75d5529fe6aa88c304750b976298039826bb3e9
mokpohans <mokpohans9029@gmail.com>
2022-04-01 11:26:03 +0900

    make file_rode function

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# 샘플 Python 스크립트입니다.
+# CSV파일 선택하기
 
-# Shift+F10을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 Shift 두 번을(를) 누릅니다.
+import os
+import pandas as pd
+from tkinter import filedialog
+from tkinter import messagebox
 
+def file_rode():
+    list_file = []                                          #파일 목록 담을 리스트 생성
+    files = filedialog.askopenfilenames(initialdir="/",title = "파일을 선택 해 주세요",filetypes = (("*.csv","*csv"), ("*.xlsx","*xlsx"), ("*.xls","*xls")))
+    #files 변수에 선택 파일 경로 넣기
 
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
-
-
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
+    if files == '':
+        messagebox.showwarning("경고", "파일을 추가 하세요")    #파일 선택 안했을 때 메세지 출력
+    return files
+__tcsv = pd.read_csv(file_rode())
+print(__tcsv)
